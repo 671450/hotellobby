d1b08c35d60d8aae487dc597525b20fe8f829e98
671450 <111427221+671450@users.noreply.github.com>
2024-05-06 18:02:07 +0200

    first commit

---
+++ HotelLobby/view/UserControls/MenuBar.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HotelLobby.view.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MenuBar.xaml
+    /// </summary>
+    public partial class MenuBar : UserControl
+    {
+
+        private void Heim(object sender, RoutedEventArgs e)
+        {
+            // Handle Heim menu click
+        }
+
+        private void Basseng(object sender, RoutedEventArgs e)
+        {
+            // Handle Basseng menu click
+        }
+
+        private void Treningsrom(object sender, RoutedEventArgs e)
+        {
+            // Handle Treningsrom menu click
+        }
+
+        private void Frukost(object sender, RoutedEventArgs e)
+        {
+            // Handle Frukost menu click
+        }
+
+        private void InnOgUt(object sender, RoutedEventArgs e)
+        {
+            // Handle InnOgUt menu click
+        }
+
+        private void Kontakt(object sender, RoutedEventArgs e)
+        {
+            // Handle InnOgUt menu click
+        }
+    }
+}
